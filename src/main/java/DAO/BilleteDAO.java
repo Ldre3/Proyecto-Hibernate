53d7cb248f5c816a221e821aebfde154d10d7fbb
Ldre3 <116470430+Ldre3@users.noreply.github.com>
2024-02-06 18:27:52 +0100

    Agregadas interfaces de DAO.

---
+++ src/main/java/DAO/BilleteDAO.java
@@ -0,0 +1,7 @@
+package DAO;
+
+public interface BilleteDAO {
+    public double calcularPrecioFinal();
+    public void reasignarAsientosFamilia();
+    public void crearBillete();
+}
---
+++ src/main/java/DAO/LogicaVueloDAO.java
@@ -0,0 +1,5 @@
+package DAO;
+
+public interface LogicaVueloDAO {
+    public void creaVuelo737();
+}
